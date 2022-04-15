61949eb67fddf5de630cbe05fd9d8f63428c57f2
Mikolaj-A-Kowalski <mikolaj.kowalski@cantab.net>
2022-04-15 15:07:28 +0200

    Add vendor sensitive compiler flag configuration

---
+++ cmake/configure_compilers.cmake
@@ -0,0 +1,36 @@
+#
+# SETS TWO VARIABLES
+#  SCONE_COMPILER_FLAGS -> FLAGS used when compiling SCONE library and executable
+#  TESTS_COMPILER_FLAGS -> FLAGS used when compiling SCONE library and executable
+#
+
+# Make sure to clean the working variables
+unset(SCONE_FLAGS_LOC CACHE)
+unset(TESTS_FLAGS_LOC CACHE)
+
+if (CMAKE_Fortran_COMPILER_ID MATCHES "GNU")
+  set(SCONE_FLAGS_LOC -std=f2008 -O3 -g -pedantic -Wall -Wno-unused-dummy-argument -cpp)
+  # Tests do not require optimisation, we also need to disable warnings
+  set(TESTS_FLAGS_LOC -std=f2008 -O0 -g -cpp)
+
+  if(COVERAGE)
+    list(APPEND SCONE_FLAGS_LOC  -coverage)
+    list(APPEND TESTS_FLAGS_LOC  -coverage)
+  endif()
+
+  if(DEBUG)
+    list(APPEND SCONE_FLAGS_LOC  -fcheck=bounds,do,mem,pointer -Waliasing)
+    list(APPEND TESTS_FLAGS_LOC  -fcheck=bounds,do,mem,pointer -Waliasing)
+  endif()
+
+else ()
+  message(STATUS ${CMAKE_Fortran_COMPILER_ID} EQUAL "GNU")
+  message(FATAL_ERROR "Trying to use unsupported Fortran compiler:
+  ${CMAKE_Fortran_COMPILER_ID}
+  ${CMAKE_Fortran_COMPILER} ${CMAKE_Fortran_COMPILER_VERSION}" )
+
+endif()
+
+# EXPORT GLOBAL Variables
+set(SCONE_COMPILER_FLAGS ${SCONE_FLAGS_LOC})
+set(TESTS_COMPILER_FLAGS ${TESTS_FLAGS_LOC})
